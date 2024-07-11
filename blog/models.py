ac0ad61c7ead92237580a8fab8cbcbf1a552e104
Elnara Shakarova <elnarashakarova@gmail.com>
2024-07-11 22:19:25 +0200

    test

--- blog/models.py
+++ blog/models.py
@@ -41,3 +41,5 @@ class Comment(models.Model):
     def __str__(self):
         return f'{self.post}'
     
+    
+    
--- blog_project/utils.py
+++ blog_project/utils.py
@@ -1,6 +1,9 @@
+
 from html_sanitizer import Sanitizer
 
 def sanitize_html(content):
     sanitizer = Sanitizer()
     
     return sanitizer.sanitize(content)
+
+##
